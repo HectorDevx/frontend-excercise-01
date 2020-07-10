695cc6218e14b32f2dba352452449a99ffe8b43f
Héctor Reyes <h.reyes@idventiva.com>
2020-07-09 19:19:52 -0500

    Updates

---
+++ precache-manifest.936946ecef28a75f248415ced03c44b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2465d55cef31d25ceaaa9b6db75c6ebd",
+    "url": "/frontend-excercise-01/index.html"
+  },
+  {
+    "revision": "35af148c8387880cf504",
+    "url": "/frontend-excercise-01/static/css/main.5ccce6be.chunk.css"
+  },
+  {
+    "revision": "566bfa511e6cdee4acb7",
+    "url": "/frontend-excercise-01/static/js/2.05c83776.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend-excercise-01/static/js/2.05c83776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35af148c8387880cf504",
+    "url": "/frontend-excercise-01/static/js/main.8adb1d0e.chunk.js"
+  },
+  {
+    "revision": "8c728ee7fe9f94142ed0",
+    "url": "/frontend-excercise-01/static/js/runtime-main.c6dca1fc.js"
+  },
+  {
+    "revision": "c97e42ddb8bbc69de3a7add651689804",
+    "url": "/frontend-excercise-01/static/media/img01.c97e42dd.png"
+  },
+  {
+    "revision": "b88d848c1d84412f1f08ecb138359d4c",
+    "url": "/frontend-excercise-01/static/media/img02.b88d848c.png"
+  },
+  {
+    "revision": "42270babcccfc6a82512dbb5337616f7",
+    "url": "/frontend-excercise-01/static/media/img03.42270bab.png"
+  },
+  {
+    "revision": "9d516ae506e606c72e9714a35fc71d57",
+    "url": "/frontend-excercise-01/static/media/logo.9d516ae5.png"
+  }
+]);
