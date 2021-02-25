18673688dfcdb7f88aaebdc4eb4a652003e6ff98
hectordevx <hectordevx@gmail.com>
2021-02-25 08:37:31 -0600

    New deploy

---
+++ docs/precache-manifest.bbd52d60c034a96bca33ba473d17cab5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980a6da87218f42891a6580868bba968",
+    "url": "/frontend-excercise-01/index.html"
+  },
+  {
+    "revision": "4b1ec79150e7aaa5a739",
+    "url": "/frontend-excercise-01/static/css/main.4b79c73a.chunk.css"
+  },
+  {
+    "revision": "566bfa511e6cdee4acb7",
+    "url": "/frontend-excercise-01/static/js/2.05c83776.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend-excercise-01/static/js/2.05c83776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1ec79150e7aaa5a739",
+    "url": "/frontend-excercise-01/static/js/main.cd5db208.chunk.js"
+  },
+  {
+    "revision": "8c728ee7fe9f94142ed0",
+    "url": "/frontend-excercise-01/static/js/runtime-main.c6dca1fc.js"
+  },
+  {
+    "revision": "c97e42ddb8bbc69de3a7add651689804",
+    "url": "/frontend-excercise-01/static/media/img01.c97e42dd.png"
+  },
+  {
+    "revision": "b88d848c1d84412f1f08ecb138359d4c",
+    "url": "/frontend-excercise-01/static/media/img02.b88d848c.png"
+  },
+  {
+    "revision": "42270babcccfc6a82512dbb5337616f7",
+    "url": "/frontend-excercise-01/static/media/img03.42270bab.png"
+  },
+  {
+    "revision": "9d516ae506e606c72e9714a35fc71d57",
+    "url": "/frontend-excercise-01/static/media/logo.9d516ae5.png"
+  }
+]);
